93c6736827f553200f2cb3c9f4208cf5dddf1baa
Neil <78010987+nwalsh1@users.noreply.github.com>
2025-07-10 13:47:32 -0400

    Finished advanced functions part 1, learned about using functions as values, callback function, anonymous functions, hoisting, callback function (passing a function as a value to another), learned about asychronous and synchronous functions - implementing setIntervcal to autoplay RPS, and learned about the forEach functin and how it is often better to loop through an array with foreach over a for loop .

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,75 @@
+const todoList = [{
+  name: 'make dinner',
+  dueDate: '2025-06-29', 
+},{
+  name: 'wash dishes',
+  dueDate: '2025-06-29', 
+}]
+
+renderTodoList();
+
+function renderTodoList(){
+  let todoListHTML = '';
+
+//using a forEach method instead of a for loop
+todoList.forEach(function(todoObject,index){
+  const {name, dueDate} = todoObject; //destructuring
+  todoListHTML += `
+    <div> ${name}</div>
+    <div> ${dueDate}</div>
+    <button onclick='
+      todoList.splice(${index},1);
+      renderTodoList();
+    ' class='delete-todo-button'>Delete</button>
+  `;
+});
+
+// for(let i = 0; i < todoList.length; i++){
+//   const todoObject = todoList[i];
+//   //const name = todoObject.name;
+//   //const dueDate = todoObject.dueDate;
+//   const {name, dueDate} = todoObject; //destructuring
+
+//   todoListHTML += `
+//     <div> ${name}</div>
+//     <div> ${dueDate}</div>
+//     <button onclick='
+//       todoList.splice(${i},1);
+//       renderTodoList();
+//     ' class='delete-todo-button'>Delete</button>
+//   `;
+//   //generating the HTMl step ^
+// }
+
+document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+}
+
+function clickAdd(){
+  const inputElement = document.querySelector('.js-input');
+  let name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    //task: task,
+    //dueDate : dueDate,
+    name,
+    dueDate // shorthand property
+  });
+  
+  inputElement.value = '';
+
+  renderTodoList();
+}
+
+//add what is in the input box to the array and then print the array tothe todoPrint paragraph element
+
+/*
+Main Idea of JavaScipt
+1. Save the data
+2. Generate the HTML
+3. Make it interactive
+
+*/
